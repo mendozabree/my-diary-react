1215820b02fa347b7f63cc757db591f518a7fb31
mendozabree <mendozabridget777@gmail.com>
2018-11-28 20:51:17 +0300

    ch(devops): setup redux and react-router-DOM
    - setup redux with the appropriate file structure
    - setup react-router-dom for the app
    
    [Delivers #162196672]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Routes from './components/routes';
 import './App.css';
+import store from './store';
 
 const App = () => (
-  <div className="App">
-    <h1>My Diary</h1>
-  </div>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
